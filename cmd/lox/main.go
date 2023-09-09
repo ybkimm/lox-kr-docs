893625bd3988dad928857971a36578d23a30076f
Daniel Caiafa <dcaiafa@istreamplanet.com>
2023-09-09 11:01:28 -0700

    -cpu-prof

--- cmd/lox/main.go
+++ cmd/lox/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	gotoken "go/token"
 	"os"
+	"runtime/pprof"
 
 	"github.com/dcaiafa/lox/internal/codegen"
 	"github.com/dcaiafa/lox/internal/errlogger"
@@ -22,6 +23,7 @@ func main() {
 func realMain() error {
 	var (
 		flagAnalyze = flag.Bool("analyze", false, "")
+		flagProf    = flag.String("cpu-prof", "", "")
 	)
 
 	flag.Parse()
@@ -30,6 +32,15 @@ func realMain() error {
 		return fmt.Errorf("<path> required")
 	}
 	dir := flag.Arg(0)
+
+	if *flagProf != "" {
+		f, err := os.Create(*flagProf)
+		if err != nil {
+			panic(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
 	fset := gotoken.NewFileSet()
 
